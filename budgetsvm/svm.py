48632c4331de5710f0e56cc0bebed100859b1086
antoniobelotti <antonio.belotti@studenti.unimi.it>
2023-03-17 14:53:38 +0100

    refactor SVC sklearn conventions
    SVC
    	- inherit from ClassifierMixin
    SVC.__init__
    	- removed unused random_state, gram, fitted from __init__
    	- removed alpha from init
    SVC.fit
    	- fit accepts any value as binary labels. Set encoder/decoder methods to map between original labels and [-1,1] labels
    	- raise ValueError if labels do not contain exactly 2 classes
    	- renamed attributes (alpha_, X_, y_) resulting from a fit() call to have a trailing underscore (used by check_is_fitted)
    	- using FitFailedWarning if training fails (checked by GridSearchCV)
    	- return self
    SVC.predict
    	 - call check_is_fitted to raise error if model has not been fitted
    	 - validate input X
    	 - use encode/decode label methods to predict and return correct labels
    SVC.score
    	- add **kwargs to make the signature same as super().score

--- budgetsvm/svm.py
+++ budgetsvm/svm.py
@@ -1,89 +1,82 @@
 import numpy as np
 
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.exceptions import FitFailedWarning
 from sklearn.metrics import accuracy_score
 
-from sklearn.utils.validation import check_X_y, check_array
-from sklearn.utils import check_random_state
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
 from budgetsvm import kernel
 from budgetsvm import optimization as opt
 
 
-class SVC(BaseEstimator):
-    def __init__(self, C=1, kernel=kernel.GaussianKernel(),
-                 budget=None, random_state=None):
+class SVC(ClassifierMixin, BaseEstimator):
+    def __init__(self, C=1, kernel=kernel.GaussianKernel(), budget=None):
         self.C = C
         self.kernel = kernel
         self.budget = budget
-        self.random_state = random_state
-        self.alpha = None
-        self.gram = None
-        self.fitted = False
 
     def __repr__(self):
         repr = 'SVM('
         if self.C != 1:
             repr += f'C={self.C}, '
-        
+
         if self.kernel != kernel.GaussianKernel():
             repr += f'kernel={self.kernel}, '
 
         if self.budget is not None:
             repr += f'budget={self.budget}, '
 
-        if self.random_state is not None:
-            repr += f'random_state={self.random_state}, '
-
         if repr[-2:] == ', ':
             repr = repr[:-2]
 
         return repr + ')'
 
-
     def fit(self, X, y, warn=False):
-        X = check_array(X)
-        y = np.array(y)
+        X, y = check_X_y(X, y)
 
-        if set(y) != {-1, 1}:
-            raise ValueError("`y` values should belong to {-1, 1}"
-                             "and both classes should be represented")
+        self.classes_, y = np.unique(y, return_inverse=True)
+        if len(self.classes_) == 1:
+            raise ValueError("Classifier can't train when only one class is present.")
+        if len(self.classes_) > 2:
+            raise ValueError("Classifier can't train when more than two classes are present.")
 
-        check_X_y(X, y)
-        self.random_state = check_random_state(self.random_state)
+        self._encode_label = np.vectorize(lambda original: -1 if original == 0 else 1)
+        self._decode_label = np.vectorize(lambda encoded: self.classes_[0] if encoded == -1 else self.classes_[1])
+        y = self._encode_label(y)
 
         solver = opt.GurobiSolver()
-        alpha = solver.solve(X, y, C=self.C, kernel=self.kernel,
-                             budget=self.budget)
+        alpha = solver.solve(X, y, C=self.C, kernel=self.kernel, budget=self.budget)
 
         sv_mask = (0 < alpha) & (alpha < self.C)
 
-        self.alpha = alpha[sv_mask]
-        self.X = X[sv_mask]
-        self.y = y[sv_mask]
-        
+        self.alpha_ = alpha[sv_mask]
+        self.X_ = X[sv_mask]
+        self.y_ = y[sv_mask]
+
         def dotprod(x_new):
-            return np.sum([a * y_i * self.kernel.compute(x, x_new)
-                           for x, y_i, a in zip(self.X, self.y, self.alpha)])
-        
-        bs = [y_i - dotprod(x) for x, y_i, a in zip(self.X, self.y,
-                                                    self.alpha)]
+            return np.sum([a * y_i * self.kernel.compute(x, x_new) for x, y_i, a in zip(self.X_, self.y_, self.alpha_)])
+
+        bs = [y_i - dotprod(x) for x, y_i, a in zip(self.X_, self.y_, self.alpha_)]
         if not bs:
-            raise RuntimeError('no SV founds')
+            raise FitFailedWarning('no SV founds')
         b = np.mean(bs)
         if warn and np.std(bs) > 1E-4:
             print('warning: computed values for b are', bs)
 
         self.df = lambda x: dotprod(x) + b
-        self.fitted = True
+        return self
 
     def decision_function(self, X):
         return np.array([self.df(x) for x in X])
 
     def predict(self, X):
-        return np.sign(self.decision_function(X))
+        check_is_fitted(self)
+        X = check_array(X)
+        encoded_label = np.sign(self.decision_function(X))
+        return self._decode_label(encoded_label)
 
-    def score(self, X, y):
+    def score(self, X, y, **kwargs):
         return accuracy_score(self.predict(X), y)
 
 
@@ -122,14 +115,15 @@ if __name__ == '__main__':
         y_neg = y[n_pos:]
         return np.array([[0.2, y] for y in y_pos] + \
                         [[0.8, y] for y in y_neg]), \
-               [1] * n_pos + [-1] * (n - n_pos)
+            [1] * n_pos + [-1] * (n - n_pos)
+
 
     X, y = generate_data(n=100)
 
     svc = SVC(C=10, kernel=LinearKernel(), budget=None)
     svc.fit(X, y)
-    print(f'classic SVM used {len(svc.alpha)} SVs.')
-        
+    print(f'classic SVM used {len(svc.alpha_)} SVs.')
+
     svc = SVC(C=10, kernel=LinearKernel(), budget=3)
     svc.fit(X, y)
-    print(f'budget SVM used {len(svc.alpha)} SVs.')
+    print(f'budget SVM used {len(svc.alpha_)} SVs.')
